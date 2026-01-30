9fbd0f6540a8594f83e68ded8a457551dbfdc936
Elflight <64664248+Elflight@users.noreply.github.com>
2026-01-30 09:04:45 +0300

    Create template.php

---
+++ rl.notes/install/components/rl/notes/templates/.default/lang/ru/template.php
@@ -0,0 +1,16 @@
+<?php
+$MESS['RL_NOTES_TITLE'] = 'Заметки';
+$MESS['RL_NOTES_PLACEHOLDER_TITLE'] = 'Заголовок';
+$MESS['RL_NOTES_PLACEHOLDER_TEXT'] = 'Текст заметки';
+$MESS['RL_NOTES_ADD_BUTTON'] = 'Добавить';
+$MESS['RL_NOTES_LOADING'] = 'Загрузка';
+$MESS['RL_NOTES_EDIT'] = 'Редактировать';
+$MESS['RL_NOTES_PLACEHOLDER_TITLE'] = 'Заголовок';
+$MESS['RL_NOTES_PLACEHOLDER_TEXT'] = 'Текст заметки';
+$MESS['RL_NOTES_SAVE'] = 'Сохранить';
+$MESS['RL_NOTES_CANCEL'] = 'Отменить';
+$MESS['RL_NOTES_CONFIRM_DELETE'] = 'Удалить заметку?';
+$MESS['RL_NOTES_ERROR_LOAD'] = 'Ошибка загрузки';
+$MESS['RL_NOTES_ERROR_CREATE'] = 'Не удалось создать заметку';
+$MESS['RL_NOTES_ERROR_UPDATE'] = 'Не удалось обновить заметку';
+$MESS['RL_NOTES_ERROR_DELETE'] = 'Не удалось удалить заметку';
