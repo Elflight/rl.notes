927b9a9f76e17efe838bda5187e93e1905ecfb31
Elflight <64664248+Elflight@users.noreply.github.com>
2026-01-30 09:08:20 +0300

    Add files via upload

---
+++ rl.notes/install/components/rl/notes/lang/ru/.parameters.php
@@ -0,0 +1,22 @@
+<?php
+$MESS['RL_NOTES_PARAM_GROUP_SETTINGS'] = 'Настройки';
+$MESS['RL_NOTES_PARAM_API_PATH'] = 'Путь к API';
+$MESS['RL_NOTES_PARAM_API_TOKEN'] = 'Токен авторизации API';
+
+// Component template messages
+$MESS['RL_NOTES_TITLE'] = 'Заметки';
+$MESS['RL_NOTES_PLACEHOLDER_TITLE'] = 'Заголовок';
+$MESS['RL_NOTES_PLACEHOLDER_TEXT'] = 'Текст';
+$MESS['RL_NOTES_ADD_BUTTON'] = 'Добавить';
+$MESS['RL_NOTES_LOADING'] = 'Загрузка...';
+$MESS['RL_NOTES_EDIT'] = 'Редактировать';
+$MESS['RL_NOTES_DELETE'] = 'Удалить';
+$MESS['RL_NOTES_SAVE'] = 'Сохранить';
+$MESS['RL_NOTES_CANCEL'] = 'Отмена';
+$MESS['RL_NOTES_CONFIRM_DELETE'] = 'Удалить заметку?';
+
+// JavaScript messages
+$MESS['RL_NOTES_ERROR_LOAD'] = 'Ошибка загрузки';
+$MESS['RL_NOTES_ERROR_CREATE'] = 'Ошибка создания заметки';
+$MESS['RL_NOTES_ERROR_UPDATE'] = 'Ошибка обновления';
+$MESS['RL_NOTES_ERROR_DELETE'] = 'Ошибка удаления';
