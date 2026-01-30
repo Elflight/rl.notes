f31eb6d583d21392eba9a424c409f2855081b7f9
Elflight <64664248+Elflight@users.noreply.github.com>
2026-01-30 09:07:32 +0300

    Create .description.php

---
+++ rl.notes/install/components/rl/notes/lang/ru/.description.php
@@ -0,0 +1,5 @@
+<?php
+$MESS['RL_NOTES_COMP_NAME'] = 'Заметки';
+$MESS['RL_NOTES_COMP_DESC'] = 'Компонент для отображения и управления заметками с использованием Vue.js';
+$MESS['RL_NOTES_COMP_GROUP_NAME'] = 'red---line';
+$MESS['RL_NOTES_COMP_GROUP_SYSTEM'] = 'Система';
